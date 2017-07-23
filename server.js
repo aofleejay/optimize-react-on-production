a034951256df1520da478e90ca9b7ac7a7d24106
kunapot <kunapot@thinknet.co.th>
2017-07-23 17:44:37 +0700

    add cache for first load

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const path = require('path')
 const compression = require('compression')
+const apicache =  require('apicache')
 
 const app = express()
 app.use(compression())
+const cache = apicache.middleware
 const bundlePath = path.join(__dirname, '/dist')
 
 app.use('/dist', express.static(bundlePath))
 
-app.get('*', (req, res) => {
+app.get('*', cache('5 minutes'), (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
